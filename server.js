6ed6238171de45d9a9bc34ff7f3ebc52079d0be0
Yasitha Gamage <yasithamaxs@gmail.com>
2021-01-22 14:35:20 +0530

    Middleware and Static Files

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
-    res.send("Hello Express :)")
-})
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+// To server static files
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (req, res) => {
+  res.render('pages/index', { pageTitle: 'Welcome' });
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
 
 app.listen(port, () => {
-    console.log(`Express Server Listning on port ${port}`)
+  console.log(`Express Server Listning on port ${port}`);
 });
