1812047c224b423c4672f8493e36e1725245578f
Yasitha Gamage <yasithamaxs@gmail.com>
2021-01-22 14:42:54 +0530

    Dedicated Routes using middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -8,14 +10,7 @@ app.set('views', path.join(__dirname, './views'));
 
 // To server static files
 app.use(express.static(path.join(__dirname, './static')));
-
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use('/', routes()); //Dedicated Routes
 
 app.listen(port, () => {
   console.log(`Express Server Listning on port ${port}`);
